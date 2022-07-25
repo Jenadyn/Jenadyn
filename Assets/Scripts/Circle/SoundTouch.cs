7c2c94e05a276d27a35007c66cb1db5d087cccaa
Jenadyn <109672502+Jenadyn@users.noreply.github.com>
2022-07-25 22:43:52 +0300

    Update SoundTouch.cs

--- Assets/Scripts/Circle/SoundTouch.cs
+++ Assets/Scripts/Circle/SoundTouch.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class SoundTouch : MonoBehaviour
 {
     [SerializeField] private AudioSource touchSFX;
+    [SerializeField] private AudioSource touchDown;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         touchSFX.Play();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Restart")
+        {
+            touchDown.Play();
+        }
     }
 }
